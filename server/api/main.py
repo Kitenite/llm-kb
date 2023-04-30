0162d76e2b8461ede8fb5e54fde261a2b764283e
Kiet Ho <hoakiet98@gmail.com>
2023-04-29 23:06:41 -0500

    Working querying

--- server/api/main.py
+++ server/api/main.py
@@ -1,22 +1,27 @@
-import time
 import redis
-from flask import Flask
+from flask import request, Flask
+from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+documents = SimpleDirectoryReader('./data').load_data()
+index = GPTSimpleVectorIndex.from_documents(documents)
 
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+@app.route("/query", methods=["POST"])
+def query_index():
+    """
+    Request body should be a JSON object with the following format:
+    {
+        "query": "What is a summary of this document?"
+    }
+    """
+
+    query_text=request.get_json(force=True) 
+    if query_text is None:
+        return "No query text provided", 400
+    
+    request_query = query_text.get("query")
+    response = index.query(request_query)
+    return str(response), 200
+
